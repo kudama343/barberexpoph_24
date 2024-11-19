2562cf1b81fa1648a9b278af0d0e99e8fe4d1bbc
kudama343 <kudama343@gmail.com>
2024-11-20 00:14:03 +0800

    Master Class template added

--- app/masterclass/page.tsx
+++ app/masterclass/page.tsx
@@ -1,19 +1,31 @@
 'use client';
 
 import NavigationBar from '../NavigationBar';
+import Image from 'next/image';
 
 const MasterClassPage: React.FC = () => {
   return (
     <div>
       <NavigationBar />
-      <main className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-800">
-        <h1 className="text-4xl font-bold mb-4">Master Class</h1>
-        <p className="text-lg">
-          Welcome to the Master Class page. This is a placeholder for Master Class content.
-        </p>
-      </main>
+
+      {/* 1st BG Section */}
+      <div className="min-h-screen bg-cover bg-center bg-[url('/images/Homepage2.png')] sm:bg-[url('/images/Homepage1.png')] flex flex-col items-center justify-center px-4 sm:px-8">
+
+      </div>
+      
+       {/* 2nd BG Section */}
+      <div className="min-h-screen bg-cover bg-center bg-[url('/images/Homepage4.png')] sm:bg-[url('/images/Homepage3.png')] flex items-center justify-center px-4 sm:px-8">
+  
+      </div>
+
+
     </div>
+
+      
+    
+    
   );
 };
+
 
 export default MasterClassPage;
