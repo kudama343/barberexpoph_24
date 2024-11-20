565145894084acfcc434ab92b77716780fb2fd9f
kudama343 <kudama343@gmail.com>
2024-11-21 01:32:32 +0800

    Responsive button on going pt2

--- app/masterclass/page.tsx
+++ app/masterclass/page.tsx
@@ -24,7 +24,16 @@ const MasterClassPage: React.FC = () => {
         </div>
       </div>
       <div className="">
-        <div className="min-h-full w-full h-screen bg-contain bg-no-repeat bg-center bg-[url('/images/MobileView/MobileViewBG7.png')] sm:bg-[url('/images/MasterClass/MasterClassBG5.png')] flex items-center justify-center px-4 sm:px-8">
+        <div className="min-h-full w-full h-screen bg-contain bg-no-repeat bg-center bg-[url('/images/MobileView/MobileViewBG7.png')] 
+                        sm:bg-[url('/images/MasterClass/MasterClassBG5.png')] flex items-center justify-center px-4 sm:px-8">
+            <div className="absolute flex flex-col  
+                    md:bottom-[120px] md:right-[50px] md:space-y-2 
+                    text-[7px] bottom-[30px] right-[20px] space-y-1
+                    lg:text-base lg:text-[15px] lg:bottom-[80px] lg:right-[160px] lg:space-y-5">
+            <button className="bg-white text-black py-3 px-6 rounded-lg shadow-md hover:bg-gray-950 hover:text-white transition duration-300 ">
+              ENROLL HERE
+            </button>
+            </div>
         </div>
       </div>
       <div className="">
@@ -35,13 +44,11 @@ const MasterClassPage: React.FC = () => {
         <div className="min-h-full w-full h-screen  flex flex-col items-center justify-center px-4 sm:px-8p relative ">
             {/* Container for the buttons */}
             <div className="absolute flex flex-col space-y-4">
-
               <div className="min-h-full w-full h-screen bg-contain bg-no-repeat bg-center bg-[url('/images/Logo.png')]  flex flex-col items-center justify-center px-4 sm:px-8p">
-              <button className="bg-white text-black py-5 px-10 rounded-3xl shadow-md text-xs hover:bg-gray-700 hover:text-white transition duration-300 sm:text-xl mt-40">
-                ENROLL HERE
-              </button>
+                <button className="bg-white text-black py-5 px-10 rounded-3xl shadow-md text-xs hover:bg-gray-700 hover:text-white transition duration-300 sm:text-xl mt-40">
+                  ENROLL HERE
+                </button>
               </div>
-              
             </div>
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -15,24 +15,24 @@ const HomePage: React.FC = () => {
       </div>
     
       <div className="">
-    <div className="min-h-full w-full h-screen bg-contain bg-no-repeat bg-center bg-[url('/images/MobileView/MobileViewBG2.png')] sm:bg-[url('/images/Homepage/HomepageBG2.png')] flex flex-col items-center justify-center px-4 sm:px-8p relative">
-    {/* Container for the buttons */}
-    <div className="absolute flex flex-col  
-                    md:bottom-[120px] md:right-[50px] md:space-y-2 
-                    text-[7px] bottom-[30px] right-[20px] space-y-1
-                    lg:text-base lg:text-[15px] lg:bottom-[80px] lg:right-[160px] lg:space-y-5">
-      {/* Top button */}
-      <button className="bg-white text-black py-3 px-6 rounded-lg shadow-md hover:bg-gray-950 hover:text-white transition duration-300 ">
-        LOOK AND LEARN HANDS-ON MASTER CLASS
-      </button>
+        <div className="min-h-full w-full h-screen bg-contain bg-no-repeat bg-center bg-[url('/images/MobileView/MobileViewBG2.png')] sm:bg-[url('/images/Homepage/HomepageBG2.png')] flex flex-col items-center justify-center px-4 sm:px-8p relative">
+          {/* Container for the buttons */}
+          <div className="absolute flex flex-col  
+                      md:bottom-[120px] md:right-[50px] md:space-y-2 
+                      text-[7px] bottom-[30px] right-[20px] space-y-1
+                      lg:text-base lg:text-[15px] lg:bottom-[80px] lg:right-[160px] lg:space-y-5">
+          {/* Top button */}
+          <button className="bg-white text-black py-3 px-6 rounded-lg shadow-md hover:bg-gray-950 hover:text-white transition duration-300 ">
+            LOOK AND LEARN HANDS-ON MASTER CLASS
+          </button>
 
-      {/* Bottom button */}
-      <button className="bg-white text-black py-3 px-6 rounded-lg shadow-md hover:bg-gray-950 hover:text-white transition duration-300">
-        IN-DEPTH PRIVATE ELITE CLASS
-      </button>
-    </div>
+          {/* Bottom button */}
+          <button className="bg-white text-black py-3 px-6 rounded-lg shadow-md hover:bg-gray-950 hover:text-white transition duration-300">
+            IN-DEPTH PRIVATE ELITE CLASS
+          </button>
+        </div>
+      </div>
   </div>
-</div>
 </div>
 
   </div>
