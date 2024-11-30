8684d1f3d50b106743dc38b2f657a85b6cc13919
louvreeeeeee <133773207+louvreeee@users.noreply.github.com>
2024-11-30 20:27:50 +0800

    Fixed Placement of Button in MasterClass & EliteClass

--- app/eliteclass/page.tsx
+++ app/eliteclass/page.tsx
@@ -8,39 +8,21 @@ const EliteClassPage: React.FC = () => {
       'https://docs.google.com/forms/d/e/1FAIpQLScSlhpxJU8wYOoKGzivK29vQaOdqmVR4eNL7fNlX4ex7GcP6A/viewform';
   };
   return (
-    <div className="">
-      
-        <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV14.png')] sm:bg-[url('/images/EliteClass/EliteClassBG1.png')] flex flex-col items-center justify-center px-4 sm:px-8p">     
-      </div>  
-        <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV15.png')] sm:bg-[url('/images/EliteClass/EliteClassBG2.png')] flex flex-col items-center justify-center px-4 sm:px-8p"> 
-      </div>    
-        <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV16.png')] sm:bg-[url('/images/EliteClass/EliteClassBG3.png')] flex flex-col items-center justify-center px-4 sm:px-8p">
+    <div className="flex flex-col">
+      <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV14.png')] sm:bg-[url('/images/EliteClass/EliteClassBG1.png')] flex flex-col items-center justify-center px-4 sm:px-8p"></div>
+      <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV15.png')] sm:bg-[url('/images/EliteClass/EliteClassBG2.png')] flex flex-col items-center justify-center px-4 sm:px-8p"></div>
+      <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV16.png')] sm:bg-[url('/images/EliteClass/EliteClassBG3.png')] flex flex-col items-center justify-center px-4 sm:px-8p"></div>
+      <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV17.png')] sm:bg-[url('/images/EliteClass/EliteClassBG1.png')] flex flex-col items-center justify-center px-4 sm:px-8p"></div>
+      <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV18.png')] sm:bg-[url('/images/EliteClass/EliteClassBG2.png')] flex flex-col items-center justify-center px-4 sm:px-8p"></div>
+      <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV19.png')] sm:bg-[url('/images/EliteClass/EliteClassBG3.png')] flex flex-col items-center justify-center px-4 sm:px-8p"></div>
+      <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV20.png')] sm:bg-[url('/images/EliteClass/EliteClassBG2.png')] flex flex-col items-center justify-center px-4 sm:px-8p"></div>
+      <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV21.png')] sm:bg-[url('/images/EliteClass/EliteClassBG3.png')] flex flex-col items-center px-4 sm:px-8p relative">
+        <button
+          onClick={redirectToForm}
+          className="absolute ml-2 bottom-[2.95in]  bg-white text-black py-[9px] px-[92px] rounded-[16px] shadow-md hover:bg-gray-950 hover:text-white transition duration-300">
+          ENROLL HERE
+        </button>
       </div>
-      <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV17.png')] sm:bg-[url('/images/EliteClass/EliteClassBG1.png')] flex flex-col items-center justify-center px-4 sm:px-8p">     
-      </div>  
-        <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV18.png')] sm:bg-[url('/images/EliteClass/EliteClassBG2.png')] flex flex-col items-center justify-center px-4 sm:px-8p"> 
-      </div>    
-        <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV19.png')] sm:bg-[url('/images/EliteClass/EliteClassBG3.png')] flex flex-col items-center justify-center px-4 sm:px-8p">
-      </div>
-        <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV20.png')] sm:bg-[url('/images/EliteClass/EliteClassBG2.png')] flex flex-col items-center justify-center px-4 sm:px-8p"> 
-      </div>    
-        <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/EliteClass/MobileV21.png')] sm:bg-[url('/images/EliteClass/EliteClassBG3.png')] flex flex-col items-center justify-center px-4 sm:px-8p">
-      </div>
-      <div className="">
-        <div
-          //className="min-h-full w-full h-screen bg-contain bg-no-repeat bg-center bg-[url('/images/MobileView/MobileViewBG7.png')] 
-          //             sm:bg-[url('/images/MasterClass/MasterClassBG5.png')] flex items-center justify-center px-4 sm:px-8">
-          className="">
-          <button
-            onClick={redirectToForm}
-            className="bg-white text-black py-3 px-6 rounded-lg shadow-md hover:bg-gray-950 hover:text-white transition duration-300">
-            ENROLL HERE
-          </button>
-        </div>
-      </div>
-      
-      
-      
     </div>
   );
 };
--- app/masterclass/page.tsx
+++ app/masterclass/page.tsx
@@ -10,29 +10,21 @@ const MasterClassPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <div className="">
+   
+      <div className="flex flex-col">
         <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/MasterClass/MobileV6.png')] sm:bg-[url('/images/MasterClass/MasterClassBG1.png')] flex flex-col items-center justify-center px-4 sm:px-8p"></div>
         <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/MasterClass/MobileV7.png')] sm:bg-[url('/images/MasterClass/MasterClassBG2.png')] flex items-center justify-center px-4 sm:px-8"></div>
         <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/MasterClass/MobileV8.png')] sm:bg-[url('/images/MasterClass/MasterClassBG3.png')] flex items-center justify-center px-4 sm:px-8"></div>
         <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/MasterClass/MobileV9.png')] sm:bg-[url('/images/MasterClass/MasterClassBG4.png')] flex items-center justify-center px-4 sm:px-8"></div>
         <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/MasterClass/MobileV10.png')] sm:bg-[url('/images/MasterClass/MasterClassBG1.png')] flex flex-col items-center justify-center px-4 sm:px-8p"></div>
         <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/MasterClass/MobileV11.png')] sm:bg-[url('/images/MasterClass/MasterClassBG2.png')] flex items-center justify-center px-4 sm:px-8"></div>
-        <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/MasterClass/MobileV12.png')] sm:bg-[url('/images/MasterClass/MasterClassBG3.png')] flex items-center justify-center px-4 sm:px-8"></div>
-        </div> 
-      <div className="">
-        <div
-          //className="min-h-full w-full h-screen bg-contain bg-no-repeat bg-center bg-[url('/images/MobileView/MobileViewBG7.png')] 
-          //            sm:bg-[url('/images/MasterClass/MasterClassBG5.png')] flex items-center justify-center px-4 sm:px-8">
-          className="">
+        <div className="min-h-full w-full h-screen bg-cover bg-no-repeat bg-center bg-[url('/images/MasterClass/MobileV12.png')] sm:bg-[url('/images/MasterClass/MasterClassBG3.png')] flex flex-col items-center px-4 sm:px-8p relative">
           <button
             onClick={redirectToForm}
-            className="bg-white text-black py-3 px-6 rounded-lg shadow-md hover:bg-gray-950 hover:text-white transition duration-300">
+            className="absolute ml-2 bottom-[2.95in]  bg-white text-black py-[9px] px-[92px] rounded-[16px] shadow-md hover:bg-gray-950 hover:text-white transition duration-300">
             ENROLL HERE
-          </button>
-        </div>
+        </ button>
       </div>
-      
     </div>
   );
 };
