f156899caee50fe709a342a93851032aa33cfba2
louvreeeeeee <133773207+louvreeee@users.noreply.github.com>
2024-12-01 11:36:13 +0800

    Final Responsive Button in Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,46 +2,40 @@ import Link from 'next/link';
 
 const HomePage: React.FC = () => {
   return (
-    <div className="">
-      {/* Background Section */}
-      <div className="">
-        <div className="min-h-full w-full h-screen sm:bg-contain bg-cover bg-no-repeat bg-center bg-black bg-[url('/images/Homepage/MobileV1.png')] flex flex-col items-center justify-center px-1"></div>
-        <div className="min-h-full w-full h-screen sm:bg-contain bg-cover bg-no-repeat bg-center bg-black bg-[url('/images/Homepage/MobileV2.png')] flex flex-col items-center justify-center px-1"></div>
-        <div className="min-h-full w-full h-screen sm:bg-contain bg-cover bg-no-repeat bg-center bg-black bg-[url('/images/Homepage/MobileV3.png')] flex flex-col items-center justify-center px-1"></div>
-        <div className="min-h-full w-full h-screen sm:bg-contain bg-cover bg-no-repeat bg-center bg-black bg-[url('/images/Homepage/MobileV5_2.png')] flex flex-col items-center justify-center px-1">
-          <div className="">
-
-            {/* Container for the buttons */}
-            <div className="flex flex-col items-center space-y-4" style={{ marginTop: '30%' }}>
-  <Link
-    href="/"
-    className="bg-[#3a3a3a] text-white text-center py-4 px-6 rounded-3xl shadow-md hover:bg-gray-950 hover:text-white transition duration-300 text-lg w-[100%] max-w-[456px] md:w-[70%] lg:w-[80%] h-[62px] text-[25px] md:text-[22px] lg:text-[18px] tracking-widest"
-    style={{ letterSpacing: '4px'}}
-  >
-    EXCLUSIVE WORKSHOPS:
-  </Link>
-  <Link
-    href="/masterclass"
-    className="bg-white text-black text-center py-1 px-4 rounded-3xl shadow-md hover:bg-gray-950 hover:text-white transition duration-300 font-montserrat-bold w-[100%] max-w-[456px] md:w-[60%] lg:w-[80%] h-[62px] text-[20px] md:text-[22px] lg:text-[18px] tracking-widest"
-    style={{ letterSpacing: '1px'}}
-  >
-    LOOK AND LEARN HANDS-ON MASTER CLASS
-  </Link>
-  <Link
-    href="/eliteclass"
-    className="bg-white text-black text-center py-4 px-4 rounded-3xl shadow-md hover:bg-gray-950 hover:text-white transition duration-300 font-montserrat-bold w-[100%] max-w-[456px] md:w-[60%] lg:w-[80%] h-[62px] text-[20px] md:text-[22px] lg:text-[18px] tracking-widest"
-    style={{ letterSpacing: '1px' }}
-  >
-    IN-DEPTH PRIVATE ELITE CLASS
-  </Link>
-</div>
-
-
-            </div>
-          </div>
-        </div>
+    <div className="flex flex-col">
+      <div className="min-h-full w-full h-screen sm:bg-contain bg-cover bg-no-repeat bg-center bg-black bg-[url('/images/Homepage/MobileV1.png')] flex flex-col items-center justify-center px-1"></div>
+      <div className="min-h-full w-full h-screen sm:bg-contain bg-cover bg-no-repeat bg-center bg-black bg-[url('/images/Homepage/MobileV2.png')] flex flex-col items-center justify-center px-1"></div>
+      <div className="min-h-full w-full h-screen sm:bg-contain bg-cover bg-no-repeat bg-center bg-black bg-[url('/images/Homepage/MobileV3.png')] flex flex-col items-center justify-center px-1"></div>
+      <div className="min-h-full w-full h-screen sm:bg-contain bg-cover bg-no-repeat bg-center bg-black bg-[url('/images/Homepage/MobileV5_2.png')] flex flex-col items-center justify-center px-1 relative">
+        <Link
+          href="/"
+          className="absolute left-1/2 transform -translate-x-1/2 bottom-[48%] bg-[#3a3a3a] text-white 
+                      py-4 rounded-3xl shadow-md hover:bg-gray-950 hover:text-white transition duration-300 
+                      text-lg w-[395px] h-[62px] text-center "
+          style={{ letterSpacing: '4px', fontSize: '22px' }}
+        >
+          EXCLUSIVE WORKSHOPS:
+        </Link>
+        <Link
+          href="/masterclass"
+          className="absolute left-1/2 transform -translate-x-1/2 bottom-[38%] bg-white text-black 
+                      py-1 rounded-3xl shadow-md hover:bg-gray-950 hover:text-white transition duration-300 
+                      font-montserrat-bold text-lg text-center"
+          style={{ width: '395px', height: '62px', letterSpacing: '1px', fontSize:'18px'}}
+        >
+          LOOK AND LEARN HANDS-ON MASTER CLASS
+        </Link>
+        <Link
+          href="/eliteclass"
+          className="absolute left-1/2 transform -translate-x-1/2 bottom-[28%] bg-white text-black 
+                      py-4 rounded-3xl shadow-md hover:bg-gray-950 hover:text-white transition duration-300 font-montserrat-bold 
+                      text-lg w-[395px] h-[62px] text-center"
+          style={{ letterSpacing: '1px', fontSize: '18px' }}
+        >
+          IN-DEPTH PRIVATE ELITE CLASS
+        </Link>
       </div>
-    
+    </div>
   );
 };
 
